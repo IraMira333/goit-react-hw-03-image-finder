0ddca869850b2f61cb13750235034f74e4c19d05
IraMira333 <irinat333@gmail.com>
2023-07-04 17:23:17 +0300

    Create ImageGallery.jsx

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './ImageGallery.module.css';
+import ImageGalleryItem from 'components/ImageGallery/ImageGalleryItem';
+
+const ImageGallery = ({ images }) => {
+  return (
+    <ul className="gallery">
+      {images.map(({ id, tags, webformatURL, largeImageURL }) => {
+        return (
+          <li className={css.photoLi} key={id}>
+            <ImageGalleryItem
+              className={css.photo}
+              tags={tags}
+              webformatURL={webformatURL}
+              largeImageURL={largeImageURL}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+ImageGallery.propTypes = {
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      tags: PropTypes.string.isRequired,
+      webformatURL: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+    })
+  ),
+};
+
+export default ImageGallery;
